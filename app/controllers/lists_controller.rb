33233d66be5dbcc1bb3d0b0e66a85bc1c0a97960
Spencer Dixon <spencer.dixon@fendixmedia.co.uk>
2014-04-15 21:27:22 +0100

    Started a simple CRUD interface to create and show lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,28 @@
 class ListsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @lists = current_user.lists
   end
 
   def new
+    @list = current_user.lists.build
   end
 
   def create
+    @list = current_user.lists.build(list_params)
+
+    if @list.save
+      flash[:success] = "List created successfully!"
+      redirect_to lists_path
+    else
+      flash[:danger] = "There was an error saving the list, please try again"
+      redirect_to lists_path
+    end
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
@@ -27,4 +38,9 @@ class ListsController < ApplicationController
 
   def share
   end
+
+  private
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
