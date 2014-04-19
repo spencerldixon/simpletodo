f556a387bcee494d1c37449e888ad8a0eaf354e6
Spencer Dixon <spencer.dixon@fendixmedia.co.uk>
2014-04-19 15:04:27 +0100

    Added validation tests for Task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 
 describe Task do
-  it "has a valid factory" do
-  	task = FactoryGirl.create(:task)
-  	expect(task).to be_valid
+  it "is valid with a name, 'completed' status, and an associated user" do
+  	expect(FactoryGirl.build(:task)).to be_valid
   end
-  
-  it "is valid with a name, 'completed' status, and an associated user"
-  it "fails validation without an associated user"
-  it "fails validation without a name"
-  it "fails validation without the 'completed' boolean"
-  it "fails validation with a duplicate name"
-  it "fails validation with a name longer than 100 characters"
+
+  it "fails validation without an associated user" do
+  	expect(FactoryGirl.build(:task, user_id: nil)).to have(1).errors_on(:user_id)
+  end
+
+  it "fails validation without a name" do
+  	expect(FactoryGirl.build(:task, name: nil)).to have(1).errors_on(:name)
+  end
+
+  it "fails validation without the 'completed' boolean" do
+  	expect(FactoryGirl.build(:task, completed: nil)).to have(1).errors_on(:completed)
+  end
+
+  it "fails validation with a duplicate name" do
+  	FactoryGirl.create(:task, name: "Example task")
+  	expect(FactoryGirl.build(:task, name: "Example task")).to have(1).errors_on(:name)
+  end
+
+  it "fails validation with a name longer than 60 characters" do
+  	expect(FactoryGirl.build(:task, name: "This is an example of a task name that is way too long to be valid")).to have(1).errors_on(:name)
+  end
 end
