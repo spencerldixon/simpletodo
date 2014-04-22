537fadfd7b3d0901e29e8eab526d371265ed74a0
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-04-22 12:59:48 +0100

    Added basic tasks CRUD, will refactor this later down the line to make creating tasks easier for users

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,18 +4,50 @@ class TasksController < ApplicationController
 	end
 	
 	def new
-		
+		@task = current_user.tasks.build
 	end
 
 	def create
+		@task = current_user.tasks.build(task_params)
+
+		if @task.save
+			flash[:success] = "Task created successfully!"
+			redirect_to @task
+		else
+			flash[:danger] = "There was an error, please try again"
+			render 'new'
+		end
 	end
 
 	def edit
+		@task = Task.find(params[:id])
 	end
 
 	def update
+		@task = Task.find(params[:id])
+
+		if @task.update_attributes(task_params)
+			flash[:success] = "Task updated successfully!"
+			redirect_to @task
+		else
+			flash[:danger] = "There was an error, please try again"
+			render 'edit'
+		end 
 	end
 
 	def destroy
+		@task = Task.find(params[:id])
+
+		if @task.present?
+			@task.destroy
+			flash[:success] = "Task deleted successfully!"
+		end
+
+		redirect_to root_path
 	end
 end
+
+private
+		def task_params
+			params.require(:task).permit(:name, :completed, :list_id)
+		end
